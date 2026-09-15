00e961d6aa732739ac9369a43fc8d442130a5b0a
saisamyuktha471 <saisamyuktha.kanaparthy@gmail.com>
2026-09-15 12:12:22 +0530

    Add files via upload

---
+++ 2520030443_Practical/Week1/Process_Execution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+int main()
+{
+    int pid;
+pid = fork();
+
+if (pid == 0)
+{
+    printf("Child PID = %d\n", getpid());
+}
+else
+{
+    printf("Parent PID = %d\n", getpid());
+    wait(NULL);
+}
+
+return 0;
+}
