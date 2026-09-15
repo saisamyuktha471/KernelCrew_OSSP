e10e37c6628caeb5380125f09113b6f3c9a49ee2
saisamyuktha471 <saisamyuktha.kanaparthy@gmail.com>
2026-09-15 09:09:19 +0530

    Update week1.c

--- 2520030443_Practical/Week1/week1.c
+++ 2520030443_Practical/Week1/week1.c
@@ -9,11 +9,11 @@ int main()
     pid_t pid;
     char command[100];
 
-    // Accept command from user
+    
     printf("Enter a Linux command: ");
     scanf("%s", command);
 
-    // Create child process
+    
     pid = fork();
 
     if (pid < 0)
@@ -24,26 +24,26 @@ int main()
 
     if (pid == 0)
     {
-        // Child process
+        
         printf("\nChild Process\n");
         printf("Child PID: %d\n", getpid());
         printf("Parent PID: %d\n", getppid());
 
-        // Execute the command
+       
         execlp(command, command, (char *)NULL);
 
-        // This executes only if exec fails
+        
         printf("Command execution failed\n");
         exit(1);
     }
     else
     {
-        // Parent process
+       
         printf("\nParent Process\n");
         printf("Parent PID: %d\n", getpid());
         printf("Waiting for child process...\n");
 
-        // Wait for child to finish
+       
         wait(NULL);
 
         printf("Child process completed.\n");
